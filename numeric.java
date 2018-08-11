236e83b1abde727ed17c30598b25ce05d525ebd1
satyachidithoti <41900189+satyachidithoti@users.noreply.github.com>
2018-08-11 06:04:37 +0100

    Create numeric.java

---
+++ numeric.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class Numeric
+{
+	public static void main(string args[])
+	{
+  try  
+  {  
+    double d = Double.parseDouble(str);  
+  }  
+  catch(NumberFormatException nfe)  
+  {  
+    return false;  
+  }  
+  return true;  
+}
+}
+	
